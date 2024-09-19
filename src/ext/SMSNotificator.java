07ba6774d2593cc30b7e18d4a4c003b0ab8f0edd
Lucifer! <6011234+rondelli@users.noreply.github.com>
2024-09-19 05:47:08 -0300

    Initial commit

---
+++ src/ext/SMSNotificator.java
@@ -0,0 +1,18 @@
+package ext;
+
+import core.Notificator;
+
+public class SMSNotificator implements Notificator {
+
+    @Override
+    public void update(Object arg) {
+        // TODO: Preguntar
+        notify(arg);
+    }
+
+    @Override
+    public void notify(Object msg) {
+        // TODO: how's this different from the other one?
+        System.out.println("Soy SMSNotificator y me notificaron: \n" + msg.toString());
+    }
+}
---
+++ src/ext/TASkOcupadoExt.java
@@ -0,0 +1,12 @@
+package ext;
+
+public class TASkOcupadoExt {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
