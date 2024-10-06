c02afa1177512378ba8cd72bc0bc677e6108fcf8
Gonza López <gonzagonzalopez20@gmail.com>
2024-10-06 18:44:20 -0300

    Use Settings.RESOURCES

--- src/ext/EmailFinder.java
+++ src/ext/EmailFinder.java
@@ -1,6 +1,7 @@
 package ext;
 
 import com.google.gson.Gson;
+import core.Settings;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -9,12 +10,10 @@ import java.util.Map;
 
 public class EmailFinder {
     
-    private static final String path = System.getProperty("user.home") + "/.TASkOcupado/Resources/Email.json";
-    
     public static Map<String, String> getEmailMap() throws IOException {
         Gson gson = new Gson();
         
-        try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(Settings.RESOURCES + "Email.json"))) {
             return gson.fromJson(reader, HashMap.class);
         }
     }
