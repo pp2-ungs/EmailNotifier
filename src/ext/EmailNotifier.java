ffe17e9044b77f27fb19e5cdd7d70cb527baeed6
Xime <xebertz@campus.ungs.edu.ar>
2024-10-20 23:09:27 -0300

    Minor fix

--- src/ext/EmailNotifier.java
+++ src/ext/EmailNotifier.java
@@ -39,7 +39,7 @@ public class EmailNotifier implements Observer {
                 System.out.println("?email not working");
             }
         }).start();
-        System.out.println("[debuggin] EmailNotifier update: \n" + event);
+        System.out.println("[debugging] EmailNotifier update: \n" + event);
     }
 
 }
